572d4aa6296c3aef40b7cdccb3c6981300a9a34f
Masahiro Nakagawa <repeatedly@gmail.com>
2020-05-20 05:21:48 +0900

    tweet.rb: update urls
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- scripts/tweet.rb
+++ scripts/tweet.rb
@@ -1,4 +1,4 @@
-require 'twitter'
+#require 'twitter'
 require 'nokogiri'
 require 'open-uri'
 
@@ -19,10 +19,10 @@ begin
   sitemap_xml = Nokogiri::XML(open(sitemap_url))
   urls = sitemap_xml.css("url loc").map { |x|
     x.text
-  }.select { |x| x !~ /blog|newsletter|events|rss/ }
+  }.select { |x| x !~ /blog|newsletter|events|rss|scribe|flume/ }
 
   # Some notable 3rd party stuff
-  urls << 'https://docs.docker.com/engine/admin/logging/fluentd/' # Docker
+  urls << 'https://docs.docker.com/config/containers/logging/fluentd/' # Docker
   urls << 'http://forums.juniper.net/t5/Analytics/Open-Source-Universal-Telemetry-Collector-for-Junos/ba-p/288677' # Juniper Networks
   urls << 'https://blog.minio.io/iot-data-storage-and-analysis-with-fluentd-minio-and-spark-26f183381183'
   urls << 'https://aws.amazon.com/blogs/aws/all-your-data-fluentd/'
@@ -30,6 +30,7 @@ begin
   urls << 'https://thenewstack.io/fluentds-role-as-a-data-collector-in-todays-cloud-native-world/'
   urls << 'https://aws.amazon.com/blogs/compute/building-a-scalable-log-solution-aggregator-with-aws-fargate-fluentd-and-amazon-kinesis-data-firehose/'
   urls << 'https://medium.com/redbox-techblog/building-an-open-data-platform-logging-with-fluentd-and-elasticsearch-4582de868398'
+  urls << 'https://blog.newrelic.com/engineering/best-practices-for-log-forwarding/'
 
   # Randomly pick one
   # TODO: weight this based on priority
