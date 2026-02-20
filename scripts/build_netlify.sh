f57cbeef76c92e571b50d1fa06c636773ff3d748
Kentaro Hayashi <kenhys@gmail.com>
2026-02-20 15:33:21 +0900

    Drop Heroku related stuff (#452)
    * views: used for Heroku contents before, now migrated to source/
    * renamed to Gemfile.middleman* to Gemfile
    * app.rb, config.ru, config/puma.rb Procfile
      * used for Sinatra application before.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- scripts/build_netlify.sh
+++ scripts/build_netlify.sh
@@ -2,12 +2,9 @@
 
 set -eu
 
-export BUNDLE_GEMFILE=Gemfile.middleman
 bundle install
 bundle exec ruby ./scripts/symlink_middleman_source.rb
 
-sed -i'' -e "s|^set :http_prefix, '/fluentd-website/'|#set :http_prefix, '/fluentd-website/'|" config.rb
-sed -i'' -e "s|^#set :http_prefix, '/'|set :http_prefix, '/'|" config.rb
 bundle exec middleman build --verbose
 
 sed -i  "s|url(/assets/plugins/parallax-slider/img|img|g" build/stylesheets/parallax-slider/parallax-slider.css
